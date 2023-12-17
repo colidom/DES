6d5a235528ffed2870c6503d6b08ed40cfd37e83
colidom <colidom@outlook.com>
2023-12-17 21:57:00 +0100

    Cambios varios en las platillas

--- Sistema_plantillas_PHP/templates/login.php
+++ Sistema_plantillas_PHP/templates/login.php
@@ -2,8 +2,12 @@
     <h1 class="display-4">Iniciar Sesión</h1>
     <form action="procesar_login.php" method="POST" class="col-md-6">
         <div class="mb-3">
-            <label for="usuario" class="form-label">Usuario:</label>
-            <input type="text" class="form-control" id="usuario" name="usuario" required>
+            <label for="user" class="form-label">Usuario:</label>
+            <input type="text" class="form-control" id="user" name="user" required>
+        </div>
+        <div class="mb-3">
+            <label for="password" class="form-label">Contraseña:</label>
+            <input type="text" class="form-control" id="password" name="password" required>
         </div>
         <!-- Agregar campos y botones según sea necesario -->
         <button type="submit" class="btn btn-primary">Entrar</button>
--- Sistema_plantillas_PHP/templates/register.php
+++ Sistema_plantillas_PHP/templates/register.php
@@ -1,18 +1,16 @@
 <section class="container">
     <h1 class="display-4">Registro</h1>
-    <form action="procesar_registro.php" method="POST" class="col-md-6">
-        <label for="usuario">Usuario:</label>
-        <input type="text" id="usuario" name="usuario" required>
+    <form>
+        <div class="form-group">
+            <label for="email">Email</label>
+            <input type="email" class="form-control" id="email" aria-describedby="emailHelp" placeholder="Enter email">
+            <small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone else.</small>
+        </div>
+        <div class="form-group">
+            <label for="exampleInputPassword1">Contraseña</label>
+            <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Password">
+        </div>
 
-        <label for="email">Email:</label>
-        <input type="email" id="email" name="email" required>
-
-        <label for="password">Contraseña:</label>
-        <input type="password" id="password" name="password" required>
-
-        <label for="confirm_password">Confirmar Contraseña:</label>
-        <input type="password" id="confirm_password" name="confirm_password" required>
-
-        <button type="submit" class="btn btn-primary">Registrar</button>
+        <button type="submit" class="btn btn-primary mt-5">Registrarse</button>
     </form>
 </section>
