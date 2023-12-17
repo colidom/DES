a2a9b11461f67ccb5cd709a3754c9c0e01d97857
colidom <colidom@outlook.com>
2023-12-17 22:09:07 +0100

    Añadidos recursos y modificadas rutas de assets

--- Sistema_plantillas_PHP/index.php
+++ Sistema_plantillas_PHP/index.php
@@ -37,7 +37,7 @@ switch ($url) {
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title><?php echo $title; ?></title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
-    <link rel="stylesheet" type="text/css" href="css/styles.css">
+    <link rel="stylesheet" type="text/css" href="assets/css/styles.css">
 </head>
 
 <body>
