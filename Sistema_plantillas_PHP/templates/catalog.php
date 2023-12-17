245e408acc13bd771a69bd5b504492bdddc86b4a
colidom <colidom@outlook.com>
2023-12-17 23:32:22 +0100

    Ajustes en catálogo

--- Sistema_plantillas_PHP/templates/catalog.php
+++ Sistema_plantillas_PHP/templates/catalog.php
@@ -1,86 +1,137 @@
 <section class="container">
-    <h1 class="display-4 catalogo"><?php echo $title ?></h1>
-    <hr>
-    <div class="row row-cols-1 row-cols-md-3 g-4">
-        <div class="col">
-            <div class="card h-100">
-                <img src="assets/img/star_wars_episode_I_phantom_menace.png" class="card-img-top" alt="...">
-                <div class="card-body">
-                    <h5 class="card-title">Star Wars Episode I</h5>
-                    <p class="card-text">Phantom Pmenace</p>
+    <h1>Catálogo</h1>
+
+    <!-- Sección de Novedades -->
+    <div class="row catalogo">
+        <div class="col-md-6">
+            <h2>Novedades</h2>
+            <div class="row">
+                <div class="col-md-4 mb-4">
+                    <div class="card catalogo--card">
+                        <img src="assets/img/star_wars_rogue_one.png" class="card-img-top" alt="...">
+                        <div class="card-body">
+                            <h5 class="card-title">Rogue one</h5>
+                            <p class="card-text">A Star Wars Story</p>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="col-md-4 mb-4">
+                    <div class="card catalogo--card">
+                        <img src="assets/img/star_wars_episode_I_phantom_menace.png" class="card-img-top" alt="...">
+                        <div class="card-body">
+                            <h5 class="card-title">Star Wars Episode I</h5>
+                            <p class="card-text">Panthom Menace</p>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="col-md-4 mb-4">
+                    <div class="card catalogo--card">
+                        <img src="assets/img/star_wars_episode_II_attack_of_the_clones.png" class="card-img-top" alt="...">
+                        <div class="card-body">
+                            <h5 class="card-title">Star Wars Episode II</h5>
+                            <p class="card-text">Attack of the clonse</p>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="col-md-4 mb-4">
+                    <div class="card catalogo--card">
+                        <img src="assets/img/star_wars_episode_III_revenge_of_the_sith.png" class="card-img-top" alt="...">
+                        <div class="card-body">
+                            <h5 class="card-title">Star Wars Episode III</h5>
+                            <p class="card-text">Revenge of the Sith</p>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="col-md-4 mb-4">
+                    <div class="card catalogo--card">
+                        <img src="assets/img/star_wars_episode_IV_a_new_hope.png" class="card-img-top" alt="...">
+                        <div class="card-body">
+                            <h5 class="card-title">Star Wars Episode IV</h5>
+                            <p class="card-text">A new hope</p>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="col-md-4 mb-4">
+                    <div class="card catalogo--card">
+                        <img src="assets/img/star_wars_episode_V_the_empire_strikes_back.png" class="card-img-top" alt="...">
+                        <div class="card-body">
+                            <h5 class="card-title">Star Wars Episode V</h5>
+                            <p class="card-text">The empire strikes back</p>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="col-md-4 mb-4">
+                    <div class="card catalogo--card">
+                        <img src="assets/img/star_wars_episode_VI_return_of_the_jedi.png" class="card-img-top" alt="...">
+                        <div class="card-body">
+                            <h5 class="card-title">Star Wars Episode VI</h5>
+                            <p class="card-text">The return of the Jedi</p>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="col-md-4 mb-4">
+                    <div class="card catalogo--card">
+                        <img src="assets/img/star_wars_episode_VII_the_force_awakens.png" class="card-img-top" alt="...">
+                        <div class="card-body">
+                            <h5 class="card-title">Star Wars Episode VII</h5>
+                            <p class="card-text">The force awakens</p>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="col-md-4 mb-4">
+                    <div class="card catalogo--card">
+                        <img src="assets/img/star_wars_episode_VIII_the_last_jedi.png" class="card-img-top" alt="...">
+                        <div class="card-body">
+                            <h5 class="card-title">Star Wars Episode VIII</h5>
+                            <p class="card-text">The last Jedi</p>
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
-        <div class="col">
-            <div class="card h-100">
-                <img src="assets/img/star_wars_episode_II_attack_of_the_clones.png" class="card-img-top" alt="...">
-                <div class="card-body">
-                    <h5 class="card-title">Star Wars Episode II</h5>
-                    <p class="card-text">Attack of the Clones</p>
+
+        <!-- Sección de Recomendaciones -->
+        <div class="col-md-6">
+            <h2>Recomendaciones</h2>
+            <div class="row catalogo">
+                <div class="col-md-4 mb-4">
+                    <div class="card catalogo--card">
+                        <img src="assets/img/star_wars_rogue_one.png" class="card-img-top" alt="...">
+                        <div class="card-body">
+                            <h5 class="card-title">Rogue one</h5>
+                            <p class="card-text">A Star Wars Story</p>
+                        </div>
+                    </div>
                 </div>
-            </div>
-        </div>
-        <div class="col">
-            <div class="card h-100">
-                <img src="assets/img/star_wars_episode_III_revenge_of_the_sith.png" class="card-img-top" alt="...">
-                <div class="card-body">
-                    <h5 class="card-title">Star Wars Episode III</h5>
-                    <p class="card-text">Revenge of the Sith</p>
+
+                <div class="col-md-4 mb-4">
+                    <div class="card catalogo--card">
+                        <img src="assets/img/star_wars_episode_I_phantom_menace.png" class="card-img-top" alt="...">
+                        <div class="card-body">
+                            <h5 class="card-title">Star Wars Episode I</h5>
+                            <p class="card-text">Panthom Menace</p>
+                        </div>
+                    </div>
                 </div>
-            </div>
-        </div>
-        <div class="col">
-            <div class="card h-100">
-                <img src="assets/img/star_wars_episode_IV_a_new_hope.png" class="card-img-top" alt="...">
-                <div class="card-body">
-                    <h5 class="card-title">Star Wars Episode IV</h5>
-                    <p class="card-text">A new hope</p>
+
+                <div class="col-md-4 mb-4">
+                    <div class="card catalogo--card">
+                        <img src="assets/img/star_wars_episode_V_the_empire_strikes_back.png" class="card-img-top" alt="...">
+                        <div class="card-body">
+                            <h5 class="card-title">Star Wars Episode V</h5>
+                            <p class="card-text">The empire strikes back</p>
+                        </div>
+                    </div>
                 </div>
-            </div>
-        </div>
-        <div class="col">
-            <div class="card h-100">
-                <img src="assets/img/star_wars_episode_V_the_empire_strikes_back.png" class="card-img-top" alt="...">
-                <div class="card-body">
-                    <h5 class="card-title">Star Wars Episode V</h5>
-                    <p class="card-text">The empire strikes back.</p>
-                </div>
-            </div>
-        </div>
-        <div class="col">
-            <div class="card h-100">
-                <img src="assets/img/star_wars_episode_VI_return_of_the_jedi.png" class="card-img-top" alt="...">
-                <div class="card-body">
-                    <h5 class="card-title">Star Wars Episode VI</h5>
-                    <p class="card-text">Return of the Jedi</p>
-                </div>
-            </div>
-        </div>
-        <div class="col">
-            <div class="card h-100">
-                <img src="assets/img/star_wars_episode_VII_the_force_awakens.png" class="card-img-top" alt="...">
-                <div class="card-body">
-                    <h5 class="card-title">Star Wars Episode VII</h5>
-                    <p class="card-text">The force awakens</p>
-                </div>
-            </div>
-        </div>
-        <div class="col">
-            <div class="card h-100">
-                <img src="assets/img/star_wars_episode_VIII_the_last_jedi.png" class="card-img-top" alt="...">
-                <div class="card-body">
-                    <h5 class="card-title">Star Wars Episode VIII</h5>
-                    <p class="card-text">The last Jedi</p>
-                </div>
-            </div>
-        </div>
-        <div class="col">
-            <div class="card h-100">
-                <img src="assets/img/star_wars_rogue_one.png" class="card-img-top" alt="...">
-                <div class="card-body">
-                    <h5 class="card-title">Rogue one</h5>
-                    <p class="card-text">A Star Wars Story</p>
-                </div>
+
             </div>
         </div>
     </div>
