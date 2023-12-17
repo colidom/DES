9b302586ee6a27bcfe64597c6abfe65e8cb1749f
colidom <colidom@outlook.com>
2023-12-17 22:49:55 +0100

    Finalizada pantalla inicio

--- Sistema_plantillas_PHP/templates/home.php
+++ Sistema_plantillas_PHP/templates/home.php
@@ -1,13 +1,58 @@
-<section class="container">
-    <h1 class="display-4">Página de Inicio</h1>
-    <div class="row">
-        <div class="col-md-6">
-            <div class="card">
-                <div class="card-body">
-                    <h2 class="card-title">Oferta especial</h2>
-                    <img src="ruta-a-la-imagen.jpg" class="card-img-top" alt="Oferta especial">
+<body>
+    <div class="container">
+        <!-- Titular principal de la página -->
+        <h1><?php echo $title; ?></h1>
+
+        <!-- Bloques de banners promocionales -->
+        <div class="row">
+            <div class="card mb-3" style="max-width: 540px;">
+                <div class="row g-0">
+                    <div class="col-md-8">
+                        <div class="card-body">
+                            <h2 class="card-title">Lorem ipsum, dolor sit amet consectetur adipisicing elit.</h2>
+                        </div>
+                    </div>
+                    <div class="col-md-4">
+                        <img src="assets/img/day_deals.jpeg" class="img-fluid rounded-start" alt="...">
+                    </div>
+                </div>
+            </div>
+            <div class="card mb-3" style="max-width: 540px;">
+                <div class="row g-0">
+                    <div class="col-md-8">
+                        <div class="card-body">
+                            <h2 class="card-title">Lorem ipsum, dolor sit amet consectetur adipisicing elit.</h2>
+                        </div>
+                    </div>
+                    <div class="col-md-4">
+                        <img src="assets/img/mando.jpeg" class="img-fluid rounded-start" alt="...">
+                    </div>
+                </div>
+            </div>
+            <div class="card mb-3" style="max-width: 540px;">
+                <div class="row g-0">
+                    <div class="col-md-8">
+                        <div class="card-body">
+                            <h2 class="card-title">Lorem ipsum, dolor sit amet consectetur adipisicing elit.</h2>
+                        </div>
+                    </div>
+                    <div class="col-md-4">
+                        <img src="assets/img/merch.jpeg" class="img-fluid rounded-start" alt="...">
+                    </div>
+                </div>
+            </div>
+            <div class="card mb-3" style="max-width: 540px;">
+                <div class="row g-0">
+                    <div class="col-md-8">
+                        <div class="card-body">
+                            <h2 class="card-title">Lorem ipsum, dolor sit amet consectetur adipisicing elit.</h2>
+                        </div>
+                    </div>
+                    <div class="col-md-4">
+                        <img src="assets/img/25_discount.jpeg" class="img-fluid rounded-start" alt="...">
+                    </div>
                 </div>
             </div>
         </div>
     </div>
-</section>
+</body>
