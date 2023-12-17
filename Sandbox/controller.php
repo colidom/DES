57f4398f494feb41f5cc3710d0047641cc73b3c6
colidom <colidom@outlook.com>
2023-12-17 01:37:19 +0100

    Hasheada password

--- Sandbox/controller.php
+++ Sandbox/controller.php
@@ -3,5 +3,19 @@
 include "./Funciones.php";
 
 if ($_SERVER['REQUEST_METHOD'] === "POST") {
-    displayDebugInfo($_POST, "SUPERGLOBAL POST");
+    $email = $_POST['email'];
+    $password = $_POST['password'];
+    $checkMeOut = $_POST['checkMeOut'];
+
+    // Hashear la contraseña
+    $hashedPassword = password_hash($password, PASSWORD_DEFAULT);
+
+    $data = [
+        'email' => $email,
+        'password' => $hashedPassword,
+        'checkMeOut' => $checkMeOut
+    ];
+
+    // Mostrar información para depuración
+    displayDebugInfo($data, "data");
 }
