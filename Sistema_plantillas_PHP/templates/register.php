d48cc66c71344d754a78f44b9884a9a23d4388a1
colidom <colidom@outlook.com>
2023-12-18 00:18:50 +0100

    Añadidos tipos a los input

--- Sistema_plantillas_PHP/templates/register.php
+++ Sistema_plantillas_PHP/templates/register.php
@@ -8,15 +8,15 @@
         </div>
         <div class="mb-3">
             <label for="email" class="form-label">Email:</label>
-            <input type="text" class="form-control" id="email" name="email" required>
+            <input type="email" class="form-control" id="email" name="email" required>
         </div>
         <div class="mb-3">
             <label for="password" class="form-label">Contraseña:</label>
-            <input type="text" class="form-control" id="password" name="password" required>
+            <input type="password" class="form-control" id="password" name="password" required>
         </div>
         <div class="mb-3">
             <label for="password2" class="form-label">Repetir contraseña:</label>
-            <input type="text" class="form-control" id="password2" name="password2" required>
+            <input type="password" class="form-control" id="password2" name="password2" required>
         </div>
         <button type="submit" class="btn btn-primary">Registrar</button>
     </form>
