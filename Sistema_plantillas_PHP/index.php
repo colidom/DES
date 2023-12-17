a16a19db0c4e1b02bb665e08dad3b68447548fac
colidom <colidom@outlook.com>
2023-12-17 22:06:35 +0100

    Mostramos el nombre de la página en la pestaña

--- Sistema_plantillas_PHP/index.php
+++ Sistema_plantillas_PHP/index.php
@@ -1,20 +1,24 @@
 <?php
 // Obtener la ruta de la URL
-$request = $_SERVER['REQUEST_URI'];
+$url = $_SERVER['REQUEST_URI'];
 
 // Enrutamiento simple basado en la URL
-switch ($request) {
+switch ($url) {
     case '/':
         $page = 'home';
+        $title = 'Inicio';
         break;
     case '/login':
         $page = 'login';
+        $title = 'Iniciar Sesión';
         break;
     case '/register':
         $page = 'register';
+        $title = 'Registrarse';
         break;
     case '/catalog':
         $page = 'catalog';
+        $title = 'Catálogo';
         break;
     default:
         // Manejo de rutas no encontradas
@@ -26,9 +30,11 @@ switch ($request) {
 ?>
 
 <!DOCTYPE html>
-<html>
+<html lang="es">
 
 <head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title><?php echo $title; ?></title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
     <link rel="stylesheet" type="text/css" href="css/styles.css">
