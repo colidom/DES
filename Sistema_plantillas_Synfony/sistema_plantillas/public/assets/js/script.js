083be7e2248488b1b8f1427b589266b9464b6965
colidom <colidom@outlook.com>
2023-12-18 01:54:44 +0100

    Añadida funcionalidad de modal al hacer click en las imágenes

---
+++ Sistema_plantillas_Synfony/sistema_plantillas/public/assets/js/script.js
@@ -0,0 +1,19 @@
+$(document).ready(function () {
+    $(".catalogo--card img").on("click", function () {
+        var imageUrl = $(this).attr("src");
+        $("#zoomedImage").attr("src", imageUrl);
+        $("#imageModal").css("display", "block");
+    });
+
+    $(".close").on("click", function () {
+        $("#imageModal").css("display", "none");
+    });
+});
+
+// Cierra la ventana modal al presionar la tecla Escape
+$(document).keyup(function (e) {
+    if (e.key === "Escape") {
+        // Verifica si la tecla presionada es Escape (ESC)
+        $(".modal").hide();
+    }
+});
