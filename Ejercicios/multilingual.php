a173b0639e3ce249b8a1d5f59c56eba283fb96e1
colidom <colidom@outlook.com>
2023-12-21 22:03:54 +0100

    Añadido bootstrap

--- Ejercicios/multilingual.php
+++ Ejercicios/multilingual.php
@@ -26,20 +26,20 @@ $textoMostrado = $textos[$lang];
 $botonMostrado = $textoBoton[$lang];
 ?>
 
+</html>
 <!DOCTYPE html>
-<html lang="<?php echo $lang; ?>">
-
+<html lang="es">
 <head>
     <meta charset="UTF-8">
-    <title>Ejemplo de Cambio de Idioma con Cookies</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="assets/css/styles.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+    <title>Multilingual</title>
 </head>
-
 <body>
-    <div class="container">
+    <div class="container text-center mt-5">
         <h1><?php echo $textoMostrado; ?></h1>
-        <a href="?lang=<?php echo ($lang == 'es') ? 'en' : 'es'; ?>" class="language-button"><?php echo $botonMostrado; ?></a>
+        <a href="?lang=<?php echo ($lang == 'es') ? 'en' : 'es'; ?>" class="btn btn-primary"><?php echo $botonMostrado; ?></a>
     </div>
 </body>
-
 </html>
