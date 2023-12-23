4f8fb370dca6a460d1ba102047f9013678ec75f4
colidom <colidom@outlook.com>
2023-12-24 00:41:47 +0100

    Añadido proyecto de migración de ejercicio de plantillas en symfony

---
+++ project_migration/src/Controller/FactorialController.php
@@ -0,0 +1,35 @@
+<?php
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
+
+/*
+ * Escribe un controlador que reciba un numero y muestre su factorial utilizadno una plantilla. La plantilla recibira el
+ * resultado y un parámetro llamado error. Si error es TRUE, en lugar del resultado hay que mostrar un mensaje apropiado.
+ */
+class FactorialController extends AbstractController
+{
+    #[Route('/factorial/{numero}')]
+    public function factorial($numero)
+    {
+        $resultado = $this->calcula_factorial($numero);
+        return $this->render('factorial/factorial.html.twig', [
+            'numero' => $numero,
+            'resultado' => $resultado
+        ]);
+    }
+
+    public function calcula_factorial($numero)
+    {
+        if ($numero < 0) {
+            return false;
+        }
+        if ($numero == 0) {
+            return 1;
+        } else {
+            return $numero * $this->calcula_factorial($numero - 1);
+        }
+
+    }
+}
