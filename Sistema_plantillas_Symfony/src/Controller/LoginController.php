b28730b8961a1b2c266c5c89aa2ea4d2a6d57845
colidom <colidom@outlook.com>
2023-12-24 16:54:00 +0100

    Controlando método POST al loguear

--- Sistema_plantillas_Symfony/src/Controller/LoginController.php
+++ Sistema_plantillas_Symfony/src/Controller/LoginController.php
@@ -2,24 +2,25 @@
 
 namespace App\Controller;
 
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class LoginController extends AbstractController
 {
-    #[Route('/login', name:'login')]
+    #[Route('/login', name: 'login')]
     public function index(): Response
     {
-        return $this->render('login/login.html.twig', [
-            'title' => 'Login',
-        ]);
-    }
 
-    #[Route('/process_login', name:'process_login')]
-    public function processLogin(Request $request): Response
-    {
-        // Aquí se procesa el inicio de sesión
-        // ...
+        if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+            // Get the login credentials from the request body
+            $username = $_POST["user"];
+            $password = $_POST["password"];
+
+        }
+
+        return $this->render('login/login.html.twig', [
+            'title' => 'Login'
+        ]);
     }
 }
